b9ff7278115e25926b682b27eb3c9af841960b95
Daniel Bider <bider.daniel@gmail.com>
2016-01-24 20:46:07 +0100

    Initial commit

---
+++ Common/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogic.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string NIP { get; set; }
+
+        public virtual AddressModel Address { get; set; }
+    }
+}
---
+++ DAL/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using UserLogic.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
